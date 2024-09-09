068504651bd85df9e37ba3bb2d7fd21479c80e50
Ahmed Essam <ahmed.e.mogy@gmail.com>
2024-09-09 18:28:50 +0300

    Update

--- DataShow/src/datashow/Configuration.java
+++ DataShow/src/datashow/Configuration.java
@@ -17,20 +17,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Configuration {
+
     private static boolean mousePressed = false;
 
-    private Map<String, String> keyValues;
+    private Map<String, Action> keyValues;
 
     public Configuration() {
         keyValues = new HashMap<>();
     }
 
-    public void setKeyValue(String key, String value) {
+    public void setKeyValue(String key, Action value) {
         keyValues.put(key, value);
     }
 
     //to take key and return value 
-    public String getKeyValue(String key) {
+    public Action getKeyValue(String key) {
         return keyValues.get(key);
     }
 
@@ -40,7 +41,7 @@ public class Configuration {
 
     public void writeToFile(String fileName) {
         try (FileWriter fileWriter = new FileWriter(fileName)) {
-            for (Map.Entry<String, String> entry : keyValues.entrySet()) {
+            for (Map.Entry<String, Action> entry : keyValues.entrySet()) {
                 fileWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
             }
         } catch (IOException e) {
@@ -57,7 +58,137 @@ public class Configuration {
                 if (parts.length == 2) {
                     String key = parts[0].trim();
                     String value = parts[1].trim();
-                    setKeyValue(key, value);/************start storing**************/
+
+                    //setKeyValue(key, value);/************start storing**************/
+                    switch (value) {
+                        case "MOUSE_RIGHT":
+                            Action mouse_right = new Mouse_Right();
+                            setKeyValue(key, mouse_right);
+                            break;
+
+                        case "MOUSE_LEFT":
+                            Action mouse_left = new Mouse_Left();
+                            setKeyValue(key, mouse_left);
+                            break;
+
+                        case "MOUSE_UP":
+                            Action mouse_up = new Mouse_Up();
+                            setKeyValue(key, mouse_up);
+                            break;
+
+                        case "MOUSE_DOWN":
+                            Action mouse_down = new Mouse_Down();
+                            setKeyValue(key, mouse_down);
+                            break;
+
+                        case "ARROW_UP":
+                            Action arrow_up = new Arrow_Up();
+                            setKeyValue(key, arrow_up);
+                            break;
+
+                        case "ARROW_DOWN":
+                            Action arrow_down = new Arrow_Down();
+                            setKeyValue(key, arrow_down);
+                            break;
+
+                        case "ARROW_RIGHT":
+                            Action arrow_right = new Arrow_Right();
+                            setKeyValue(key, arrow_right);
+                            break;
+
+                        case "ARROW_LEFT":
+                            Action arrow_left = new Arrow_Left();
+                            setKeyValue(key, arrow_left);
+                            break;
+
+                        case "LEFT_CLICK":
+                            Action left_click = new Left_Click();
+                            setKeyValue(key, left_click);
+                            break;
+
+                        case "PRTSC":
+                            Action prtsc = new Print_Screen();
+                            setKeyValue(key, prtsc);
+                            break;
+
+                        case "TOGGLE_PRESENTATION":
+                            Action toggle_presentation = new Toggle_Presentation();
+                            setKeyValue(key, toggle_presentation);
+                            break;
+
+                        case "SCROLL_UP":
+                            Action scroll_up = new Scroll_Up();
+                            setKeyValue(key, scroll_up);
+                            break;
+
+                        case "SCROLL_DOWN":
+                            Action scroll_down = new Scroll_Down();
+                            setKeyValue(key, scroll_down);
+                            break;
+
+                        case "TOGGLE_HOLD":
+                            Action toggle_hold = new Toggle_Hold();
+                            setKeyValue(key, toggle_hold);
+                            break;
+
+                        case "PRESS_0":
+                            Action press_0 = new Print_0();
+                            setKeyValue(key, press_0);
+                            break;
+
+                        case "PRESS_1":
+                            Action press_1 = new Print_1();
+                            setKeyValue(key, press_1);
+                            break;
+
+                        case "PRESS_2":
+                            Action press_2 = new Print_2();
+                            setKeyValue(key, press_2);
+                            break;
+
+                        case "PRESS_3":
+                            Action press_3 = new Print_3();
+                            setKeyValue(key, press_3);
+                            break;
+
+                        case "PRESS_4":
+                            Action press_4 = new Print_4();
+                            setKeyValue(key, press_4);
+                            break;
+
+                        case "PRESS_5":
+                            Action press_5 = new Print_5();
+                            setKeyValue(key, press_5);
+                            break;
+
+                        case "PRESS_6":
+                            Action press_6 = new Print_6();
+                            setKeyValue(key, press_6);
+                            break;
+
+                        case "PRESS_7":
+                            Action press_7 = new Print_7();
+                            setKeyValue(key, press_7);
+                            break;
+
+                        case "PRESS_8":
+                            Action press_8 = new Print_8();
+                            setKeyValue(key, press_8);
+                            break;
+
+                        case "PRESS_9":
+                            Action press_9 = new Print_9();
+                            setKeyValue(key, press_9);
+                            break;
+
+                        case "CLOSE":
+                            Action close = new Close();
+                            setKeyValue(key, close);
+                            break;
+
+                        default:
+                            break;
+                    }
                 }
             }
         } catch (IOException e) {
@@ -65,4 +196,3 @@ public class Configuration {
         }
     }
 }
-
--- DataShow/src/datashow/DataShow.java
+++ DataShow/src/datashow/DataShow.java
@@ -2,257 +2,13 @@
 
 //import datashow.Configuration;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.awt.AWTException;
-import java.awt.Point;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.awt.event.InputEvent;
-import java.awt.MouseInfo;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.*;
+import java.io.InputStreamReader;
 
 public class DataShow {
 
-    public static void act(String action) {
-        Robot robot;
-        try {
-            robot = new Robot();
-
-            Point currentPosition;
-            int currentX, currentY;
-            boolean presenting = false;
-            boolean holding = false;
-            switch (action) {
-                case "MOUSE_RIGHT":
-                    for (int i = 0; i < 30; i++) {
-                        currentPosition = MouseInfo.getPointerInfo().getLocation();
-                        currentX = (int) currentPosition.getX();
-                        currentY = (int) currentPosition.getY();
-                        robot.mouseMove(currentX + 1, currentY);
-                    }
-                    break;
-
-                case "MOUSE_LEFT":
-                    for (int i = 0; i < 30; i++) {
-                        currentPosition = MouseInfo.getPointerInfo().getLocation();
-                        currentX = (int) currentPosition.getX();
-                        currentY = (int) currentPosition.getY();
-                        robot.mouseMove(currentX - 1, currentY);
-                    }
-                    break;
-
-                case "MOUSE_UP":
-                    for (int i = 0; i < 30; i++) {
-                        currentPosition = MouseInfo.getPointerInfo().getLocation();
-                        currentX = (int) currentPosition.getX();
-                        currentY = (int) currentPosition.getY();
-                        robot.mouseMove(currentX, currentY - 1);
-                    }
-                    break;
-
-                case "MOUSE_DOWN":
-                    for (int i = 0; i < 30; i++) {
-                        currentPosition = MouseInfo.getPointerInfo().getLocation();
-                        currentX = (int) currentPosition.getX();
-                        currentY = (int) currentPosition.getY();
-                        robot.mouseMove(currentX, currentY + 1);
-                    }
-                    break;
-
-                case "ARROW_UP":
-                    robot.keyPress(KeyEvent.VK_UP);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_UP);
-                    holding = false;
-                    break;
-
-                case "ARROW_DOWN":
-                    robot.keyPress(KeyEvent.VK_DOWN);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_DOWN);
-                    holding = false;
-                    break;
-
-                case "ARROW_RIGHT":
-                    robot.keyPress(KeyEvent.VK_RIGHT);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_RIGHT);
-                    holding = false;
-                    break;
-
-                case "ARROW_LEFT":
-                    robot.keyPress(KeyEvent.VK_LEFT);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_LEFT);
-                    holding = false;
-                    break;
-
-                case "LEFT_CLICK":
-                    robot.mousePress(InputEvent.BUTTON1_MASK);
-                    robot.delay(5);
-                    robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                    holding = false;
-                    break;
-
-                case "PRTSC":
-                    System.out.println("KEY_2 was pressed! Taking screenshot...");
-
-                    // Get the screen dimensions for the capture
-                    Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-
-                    // Capture the screen
-                    BufferedImage capture = robot.createScreenCapture(screenRect);
-
-                    // Save the screenshot to a file
-                    try {
-                        ImageIO.write(capture, "png", new File("/home/root/screenshot.png"));
-                        System.out.println("Screenshot saved as screenshot.png");
-                    } catch (IOException e) {
-                        System.out.println("Failed to save screenshot: " + e.getMessage());
-                        e.printStackTrace();
-                    }
-                    holding = false;
-                    break;
-
-                case "TOGGLE_PRESENTATION":
-                    if (!presenting) {
-                        robot.keyPress(KeyEvent.VK_F5);
-                        robot.delay(10);
-                        robot.keyRelease(KeyEvent.VK_F5);
-//                        Thread.sleep(500);
-                        presenting = true;
-                    } else {
-                        robot.keyPress(KeyEvent.VK_ESCAPE);
-                        robot.delay(10);
-                        robot.keyRelease(KeyEvent.VK_ESCAPE);
-//                        Thread.sleep(500);
-                        presenting = false;
-                    }
-                    holding = false;
-                    break;
-
-                case "SCROLL_UP":
-                    robot.mouseWheel((-5));
-                    robot.delay(1000);
-                    break;
-
-                case "SCROLL_DOWN":
-                    robot.mouseWheel((5));
-                    robot.delay(1000);
-                    break;
-
-                case "TOGGLE_HOLD":
-                    if (!holding) {
-                        robot.mousePress(InputEvent.BUTTON1_MASK);
-                        robot.delay(5);
-
-                        holding = true;
-
-                    } else {
-                        robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                        robot.delay(5);
-                        holding = false;
-                    }
-                    break;
-                    
-                case "PRINT0":
-                    robot.keyPress(KeyEvent.VK_0);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_0);
-                    holding = false;
-                    break;
-                    
-                case "PRINT1":
-                    robot.keyPress(KeyEvent.VK_1);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_1);
-                    holding = false;
-                    break;
-                    
-                case "PRINT2":
-                    robot.keyPress(KeyEvent.VK_2);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_2);
-                    holding = false;
-                    break;
-                    
-                case "PRINT3":
-                    robot.keyPress(KeyEvent.VK_3);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_3);
-                    holding = false;
-                    break;
-                    
-                case "PRINT4":
-                    robot.keyPress(KeyEvent.VK_4);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_4);
-                    holding = false;
-                    break;
-
-                case "PRINT5":
-
-                    robot.keyPress(KeyEvent.VK_5);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_5);
-                    holding = false;
-                    break;
-                case "PRINT6":
-
-                    robot.keyPress(KeyEvent.VK_6);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_6);
-                    holding = false;
-                    break;
-                case "PRINT7":
-
-                    robot.keyPress(KeyEvent.VK_7);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_7);
-                    holding = false;
-                    break;
-                case "PRINT8":
-
-                    robot.keyPress(KeyEvent.VK_8);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_8);
-                    holding = false;
-                    break;
-                    
-                case "PRINT9":
-                    robot.keyPress(KeyEvent.VK_9);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_9);
-                    holding = false;
-                    break;
-                    
-                case "CLOSE":
-                    robot.keyPress(KeyEvent.VK_7);
-                    robot.delay(10);
-                    robot.keyRelease(KeyEvent.VK_7);
-                    holding = false;
-                    break;
-                    
-                default:
-                    break;
-            }
-
-        } catch (AWTException ex) {
-            Logger.getLogger(DataShow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
     public static void main(String[] args) {
         Configuration config = new Configuration();
-//        config.loadFromFile("/home/configuration/remoteConfig.txt");  //load key from file
         config.loadFromFile("/home/configuration/sudoku.txt");  //load key from file
 
         String LastCommand = "";
@@ -263,7 +19,8 @@ public class DataShow {
             String line;
 
             System.out.println("Listening for IR key 44...");
-            String action = "";
+            Action action = null;
+
             while ((line = reader.readLine()) != null) {
                 if (!line.equals(LastCommand)) {
                     LastCommand = line;
@@ -331,18 +88,17 @@ public class DataShow {
 
                     } else if (line.contains("00 KEY_9")) {
                         action = config.getKeyValue("KEY_9");
-
                     }
 
-                    act(action);
-                    action = "";
-
+                    if (action != null) {
+                        action.perform();
+                        action = null;
+                    }
                 }
             }
 //                Thread.sleep(3000);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
         }
     }
 }
